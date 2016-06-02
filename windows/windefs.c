6f715b2b08f3c6b10531c5361baece11536afeaa
Seokju Yun <noricube@gmail.com>
2016-06-02 11:16:55 +0900

    fontspec이 MB로 폰트 이름을 저장하고 있어 파일 인코딩을 변경하니 폰트이름이 깨지던 문제 수정 WC로 폰트명을 기록해두고 한국어 환경일때만 변환해서 폰트를 지정하도록 수정

--- windows/windefs.c
+++ windows/windefs.c
@@ -12,14 +12,22 @@ FontSpec *platform_default_fontspec(const char *name)
     versioninfo.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
     GetVersionEx(&versioninfo);
 
+	int acp = GetACP();
+
 	if (!strcmp(name, "Font")) {
 		if (versioninfo.dwMajorVersion >= 6)
 			return fontspec_new("Consolas", 0, 10, ANSI_CHARSET);
 		else
 			return fontspec_new("Courier New", 0, 10, ANSI_CHARSET);
 	}
-	else if (!strcmp(name, "FontUnicode")) {
-		return fontspec_new("나눔고딕코딩", 0, 10, HANGEUL_CHARSET);
+	else if (!strcmp(name, "FontUnicode") && acp == 949) { // code page가 한국어 일때만 나눔고딕 코딩으로 셋팅
+		wchar_t *unicode_font = L"나눔고딕코딩";
+		char* unicode_font_mb = short_wc_to_mb(acp, 0, unicode_font, wcslen(unicode_font), NULL, NULL);
+
+		FontSpec* fontspec = fontspec_new(unicode_font_mb, 0, 10, HANGEUL_CHARSET);
+		free(unicode_font_mb);
+
+		return fontspec;
 	}
 	else {
 		return fontspec_new("", 0, 0, 0);
