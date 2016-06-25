ec048deb975466fd7922dccc1909f13662753f09
nori Seokju Yun <noricube@gmail.com>
2016-06-26 02:34:29 +0900

    Localization을 위한 Dialog 리소스 추가

---
+++ windows/win_res_localization.h
@@ -0,0 +1,15 @@
+/*
+ * win_res.h - constants shared between win_res.rc2 and the C code.
+ */
+
+#ifndef PUTTY_WIN_RES_LOCALIZATION_H
+#define PUTTY_WIN_RES_LOCALIZATION_H
+
+
+#define IDD_MAINBOXL      10102
+#define IDD_LOGBOXL       10110
+#define IDD_ABOUTBOXL     10111
+#define IDD_RECONFL       10112
+#define IDD_LICENCEBOXL   10113
+
+#endif
